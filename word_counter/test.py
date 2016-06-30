f35424362387b118607abbb555264e75b43e22c3
Luis Antonio Vieira Junior <lvieira@lvieira.com>
2016-06-30 11:59:23 -0300

    word_counter solution

--- word_counter/test.py
+++ word_counter/test.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 import unittest
 
-from airplane import solution
+from word_counter import solution
 
 __all__ = [
     'TestWordCounter',
@@ -12,8 +12,35 @@ __all__ = [
 
 class TestWordCounter(unittest.TestCase):
 
-    def test_fail(self):
-        self.assertEqual(solution(''), tuple())
+    def test_empty_string_returns_empty_tuple(self):
+        self.assertEqual(solution(''), ([], []))
+
+    def test_string_with_one_word_returns_same_word_as_least_and_most_common(self):
+        self.assertEqual(solution('a'), ([('a', 1)], [('a', 1)]))
+
+    def test_string_with_one_word_returns_same_word_as_least_and_most_common_2(self):
+        self.assertEqual(solution('b'), ([('b', 1)], [('b', 1)]))
+
+    def test_string_with_one_word_returns_same_word_as_least_and_most_common_3(self):
+        self.assertEqual(solution('c'), ([('c', 1)], [('c', 1)]))
+
+    def test_string_with_one_word_returns_same_word_as_least_and_most_common_4(self):
+        self.assertEqual(solution('aa'), ([('aa', 1)], [('aa', 1)]))
+
+    def test_string_with_two_equal_words_returns_same_word_as_least_and_most_common(self):
+        self.assertEqual(solution('a a'), ([('a', 2)], [('a', 2)]))
+
+    def test_string_with_three_equal_words_returns_same_word_as_least_and_most_common(self):
+        self.assertEqual(solution('a a a'), ([('a', 3)], [('a', 3)]))
+
+    def test_string_with_two_differents_words_returns_two_different_words_with_same_count(self):
+        self.assertEqual(solution('a b'), ([('a', 1), ('b', 1)], [('a', 1), ('b', 1)]))
+
+    def test_string_with_three_words_one_different_and_two_equal_returns_the_correct_list(self):
+        self.assertEqual(solution('a a b'), ([('b', 1), ('a', 2)], [('a', 2), ('b', 1)]))
+
+    def test_string_with_four_words_two_equal_returns_the_correct_list(self):
+        self.assertEqual(solution('a a b b'), ([('a', 2), ('b', 2)], [('a', 2), ('b', 2)]))
 
 
 if __name__ == '__main__':
--- word_counter/word_counter.py
+++ word_counter/word_counter.py
@@ -1,6 +1,9 @@
 # -*- coding: UTF-8 -*-
 from __future__ import unicode_literals
 
+import collections
+import re
+
 __all__ = [
     'solution',
 ]
@@ -18,8 +21,8 @@ Each word of this text is a word in the english language
 
 The result should be:
 (
-    [('has', 1), ('words', 1), ('that', 1), ('are', 1), ('part', 1)],
-    [('this', 4), ('text', 4), ('is', 2), ('a', 2), ('word', 2)],
+    [('are', 1), ('has', 1), ('words', 1), ('part', 1), ('that', 1)],
+    [('text', 4), ('this', 4), ('a', 2), ('is', 2), ('word', 2)],
 )
 
 Example text: http://pastebin.com/raw/xkTYv7nf
@@ -27,4 +30,10 @@ Example text: http://pastebin.com/raw/xkTYv7nf
 
 
 def solution(text):
-    return 0
+    word_counter = collections.Counter(re.findall(r'\w+', text.lower()))
+    most_common = word_counter.most_common(len(word_counter))
+
+    return (
+        sorted(most_common[-5:], key=lambda t: (t[1], t[0])),
+        sorted(most_common[:5], key=lambda t: (-t[1], t[0]))
+    )
